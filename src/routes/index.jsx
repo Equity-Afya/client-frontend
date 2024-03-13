f7d1016c5e549270eec686a73c925e1bc244b889
Baraka <raphaelbaraka424@gmail.com>
2024-03-13 08:50:05 +0300

    modern-lab changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -14,6 +14,7 @@ import CustomerCarePage from "../pages/CustomerCarePage";
 import TeleclinicsPage from "../pages/TeleclinicsPage";
 import OtherServicesPage from "../pages/OtherServicesPage";
 import ForgotPasswordPage from "../pages/ForgotPasswordPage";
+import ModernLabPage from "../pages/ModernLabPage";
 
 function AppRoutes() {
   return (
@@ -34,7 +35,7 @@ function AppRoutes() {
         <Route path="/customer-care" element={< CustomerCarePage />} />
         <Route path="/other-services" element={< OtherServicesPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        
+        <Route path="/modern-lab" element={<ModernLabPage />} />
       </Routes>
     </Router>
   );
