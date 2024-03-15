8f0551758485cd4800278a6732d9669f4449b3a2
Mbarak-jr <mohammedmbarak77@gmail.com>
2024-03-15 10:44:03 +0300

    linked forgot-password with login page

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -121,7 +121,7 @@ function LoginForm() {
 
         {/* Forgot password link */}
         <Typography variant="body1" sx={{ textAlign: 'right',marginLeft:20 }}>
-          <Link href="/" underline="none">Forgot password?</Link>
+          <Link href="/forgot-password" underline="none">Forgot password?</Link>
         </Typography>
         </Box>
 
