50acfab107dbe3d33e9879a0234307224fb8d54b
Victortimbwa <victortimbwa@gmail.com>
2024-03-18 13:08:48 +0300

    Updated Fullscreen

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,6 +6,7 @@ import {
   ListItemText,
   CssBaseline,
   IconButton,
+  Box,
 } from "@mui/material";
 import {
   Dashboard,
@@ -62,128 +63,135 @@ function Sidebar() {
   return (
     <>
       <CssBaseline />
-      <List
+      <Box
         sx={{
-          width: 240,
-          height: "100%",
-          backgroundColor: brightnessMode ? "#FFFFFF" : "#C00100",
-          color: brightnessMode ? "#000000" : "white",
-          paddingTop: 0,
+          display: "flex",
+          flexDirection: "column",
+          height: fullscreenMode ? "100vh" : "auto",
         }}
       >
-        {/* Main Menu Items */}
-        <ListItem button onClick={() => handleNavigation("/dashboard")}>
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
-          >
-            <Dashboard />
-          </ListItemIcon>
-          <ListItemText primary="Dashboard" />
-          {/* Add Fullscreen Button */}
-          <IconButton
-            onClick={toggleFullscreen}
-            sx={{ color: brightnessMode ? "#000000" : "white" }}
-          >
-            {fullscreenMode ? <FullscreenExit /> : <Fullscreen />}
-          </IconButton>
-        </ListItem>
-        <ListItem button onClick={() => handleNavigation("/appointments")}>
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
-          >
-            <EventNote />
-          </ListItemIcon>
-          <ListItemText primary="My Appointments" />
-        </ListItem>
-        <ListItem button onClick={() => handleNavigation("/specialists")}>
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
-          >
-            <PeopleAlt />
-          </ListItemIcon>
-          <ListItemText primary="Specialists" />
-        </ListItem>
-        <ListItem button onClick={() => handleNavigation("/health-records")}>
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
-          >
-            <History />
-          </ListItemIcon>
-          <ListItemText primary="My History" />
-        </ListItem>
-        <ListItem button onClick={() => handleNavigation("/payments")}>
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
-          >
-            <Payment />
-          </ListItemIcon>
-          <ListItemText primary="Payments" />
-        </ListItem>
-        <ListItem button onClick={() => handleNavigation("/customer-care")}>
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
-          >
-            <Headset />
-          </ListItemIcon>
-          <ListItemText primary="Customer Care" />
-        </ListItem>
-        <ListItem button onClick={() => handleNavigation("/my-account")}>
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
-          >
-            <AccountCircle />
-          </ListItemIcon>
-          <ListItemText primary="My Account" />
-        </ListItem>
-        {/* Additional Items */}
-        <ListItem
-          button
-          sx={{ paddingTop: 22 }}
-          onClick={() => handleNavigation("/light-mode")}
+        <List
+          sx={{
+            flex: 1,
+            backgroundColor: brightnessMode ? "#FFFFFF" : "#C00100",
+            color: brightnessMode ? "#000000" : "white",
+            paddingTop: 0,
+          }}
         >
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
+          {/* Main Menu Items */}
+          <ListItem button onClick={() => handleNavigation("/dashboard")}>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              <Dashboard />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+            {/* Fullscreen Button */}
+            <IconButton
+              onClick={toggleFullscreen}
+              sx={{ color: brightnessMode ? "#000000" : "white" }}
+            >
+              {fullscreenMode ? <FullscreenExit /> : <Fullscreen />}
+            </IconButton>
+          </ListItem>
+          <ListItem button onClick={() => handleNavigation("/appointments")}>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              <EventNote />
+            </ListItemIcon>
+            <ListItemText primary="My Appointments" />
+          </ListItem>
+          <ListItem button onClick={() => handleNavigation("/specialists")}>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              <PeopleAlt />
+            </ListItemIcon>
+            <ListItemText primary="Specialists" />
+          </ListItem>
+          <ListItem button onClick={() => handleNavigation("/health-records")}>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              <History />
+            </ListItemIcon>
+            <ListItemText primary="My History" />
+          </ListItem>
+          <ListItem button onClick={() => handleNavigation("/payments")}>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              <Payment />
+            </ListItemIcon>
+            <ListItemText primary="Payments" />
+          </ListItem>
+          <ListItem button onClick={() => handleNavigation("/customer-care")}>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              <Headset />
+            </ListItemIcon>
+            <ListItemText primary="Customer Care" />
+          </ListItem>
+          <ListItem button onClick={() => handleNavigation("/my-account")}>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              <AccountCircle />
+            </ListItemIcon>
+            <ListItemText primary="My Account" />
+          </ListItem>
+          {/* Additional Items */}
+          <ListItem
+            button
+            sx={{ paddingTop: 22 }}
+            onClick={() => handleNavigation("/light-mode")}
           >
-            {brightnessMode ? <Brightness7 /> : <Brightness4 />}
-          </ListItemIcon>
-          <ListItemText primary="Light Mode" />
-        </ListItem>
-        <ListItem button onClick={handleLogout}>
-          <ListItemIcon
-            sx={{
-              color: brightnessMode ? "#000000" : "white",
-              marginRight: -3,
-            }}
-          >
-            <Logout />
-          </ListItemIcon>
-          <ListItemText primary="Logout" />
-        </ListItem>
-      </List>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              {brightnessMode ? <Brightness7 /> : <Brightness4 />}
+            </ListItemIcon>
+            <ListItemText primary="Light Mode" />
+          </ListItem>
+          <ListItem button onClick={handleLogout}>
+            <ListItemIcon
+              sx={{
+                color: brightnessMode ? "#000000" : "white",
+                marginRight: -3,
+              }}
+            >
+              <Logout />
+            </ListItemIcon>
+            <ListItemText primary="Logout" />
+          </ListItem>
+        </List>
+      </Box>
     </>
   );
 }
