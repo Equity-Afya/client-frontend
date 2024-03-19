505ab9331f9a23e6bbdcf48abd07d72148fd573b
Mbarak-jr <mohammedmbarak77@gmail.com>
2024-03-19 13:25:51 +0300

    added loading spinner and show password eye icon

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -3,19 +3,26 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import axios from 'axios';
 import Checkbox from '@mui/material/Checkbox';
 import { Link, useNavigate } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); // State variable to toggle password visibility
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
     try {
       const response = await axios.post('https://8ed2-102-210-244-74.ngrok-free.app/api/patient/login', {
@@ -23,9 +30,8 @@ function Login() {
         password
       });
 
-      alert(response.data.message); // Display login status message
+      alert(response.data.message);
 
-      // Redirect to Home page on successful login
       if (response.status === 200) {
         navigate('/dashboard');
       }
@@ -36,7 +42,13 @@ function Login() {
         setErrorMessage('Login failed. Please check your credentials.');
       }
       console.error('Login Error:', error);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -69,20 +81,27 @@ function Login() {
               label="Email"
               variant="outlined"
               placeholder='Enter your email'
-              required // Marked as required
+              required
               fullWidth
             /><br /><br />
           </div>
           <div>
             <TextField
-              type='password'
+              type={showPassword ? 'text' : 'password'} // Toggle password visibility based on showPassword state
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               label="Password"
               variant="outlined"
               placeholder='Enter your password'
-              required // Marked as required
+              required
               fullWidth
+              InputProps={{
+                endAdornment: (
+                  <IconButton onClick={handleTogglePasswordVisibility} edge="end">
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                ),
+              }}
             /><br /><br />
           </div>
           <div style={{ display: 'flex', alignItems: 'center' }}>
@@ -95,7 +114,9 @@ function Login() {
             <Link to='/forgot-password' style={{ textDecoration: 'none', marginLeft: 'auto', color: '#c00100' }}>Forgot password?</Link>
           </div>
           <br />
-          <Button type='submit' variant="contained" sx={{ backgroundColor: '#c00100', width: '300px' }}>Login</Button>
+          <Button type='submit' variant="contained" sx={{ backgroundColor: '#c00100', width: '300px' }}>
+            {loading ? <CircularProgress size={24} /> : 'Login'}
+          </Button>
           {errorMessage && <div className="error-message">{errorMessage}</div>}
         </form><br />
         <div>
@@ -109,4 +130,3 @@ function Login() {
 }
 
 export default Login;
-
