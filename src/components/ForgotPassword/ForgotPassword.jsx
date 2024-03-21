b8edf0a24a29de2b83ea6dc59b76ebaf7816a68f
Vailance307 <vailancemkanjumwa@gmail.com>
2024-03-21 09:37:59 +0300

    correction

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -15,17 +15,11 @@ function ForgotPassword() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true); 
+    
     try {
-      const response = await axios.post(
-        "https://062d-102-210-244-74.ngrok-free.app/api/patient/forgotpassword",
-        {
-          email,
-        }
+      const response = await axios.post('https://d3a9-102-210-244-74.ngrok-free.app/api/patient/forgotpassword', 
+        {email}
       );
-  
-      const response = await axios.post('https://d3a9-102-210-244-74.ngrok-free.app/api/patient/forgotpassword', {
-        email
-      });
       alert(response.data.message);
   
       if (response.status === 200) {
