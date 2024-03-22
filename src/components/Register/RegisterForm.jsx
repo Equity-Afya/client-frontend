0b3d49b8e945fef39a6c325173ec5aa0d46e3ed6
Sammy maina <sammymainakirathe@gmail.com>
2024-03-22 11:27:37 +0300

    updated base URLfor Register page

--- src/components/Register/RegisterForm.jsx
+++ src/components/Register/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import api from "./api";
+import api from "../../services/api";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
